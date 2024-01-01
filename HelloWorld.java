690fbda6332422392cee8aff5a0fbd6f16c1ac86
skykingviking <30716126-skykingviking@users.noreply.replit.com>
2024-01-01 08:07:17 +0000

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
